ba1282d97ca1c180289fcd6427663f990e7d08cb
azuma46 <time46.les@gmail.com>
2019-03-07 14:47:05 +0900

    更新
    timerの追加
    ビルドの更新

--- katamari/Assets/Scripts/Game_Manager.cs
+++ katamari/Assets/Scripts/Game_Manager.cs
@@ -6,22 +6,38 @@ using UnityEngine.UI;
 public class Game_Manager :MonoBehaviour {
 
     public Text now_level;
-    //public Text timer;
+    public Text timer;
+
+    public int time;
+    private int min;
+    private float sec;
+
+    public int clear_level;
 
     private GameObject getPlayer;
     private int level = -1;
 
     void Start () {
+        min = time / 60;
+        sec = time % 60;
         now_level.text = "";
         getPlayer = GameObject.Find("Player");
     }
 
 	void Update () {
-        level = getPlayer.gameObject.GetComponent<Player>().getObject_Level();
-        now_level.text = level.ToString();
+        level = getPlayer.gameObject.GetComponent<Player>().getObject_Level();//playerのレベルを更新
+        now_level.text = level.ToString();//playerのレベルを更新
 
         //ESCでゲームを終了
         if (Input.GetKey(KeyCode.Escape)) Application.Quit();
 
+        sec -= Time.deltaTime;
+        if (sec <= 0)
+        {
+            min--;
+            sec = 59f;
+        }
+
+        timer.text = min.ToString("00") + ":" + sec.ToString("00");
     }
 }
